3dfed5aec3d7ff9d0b08dfb49e9693300fe754c0
lennon <1099571219@qq.com>
2022-02-09 08:59:12 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>1</div>
+  <test/>
+</template>
+
+<script>
+import test from './components/test.vue'
+
+export default {
+  name: 'App',
+  components: {
+    test
+  }
+}
+</script>
