735ae39f2e8ab74fc0ce67bd215ac30974c13810
lennon <1099571219@qq.com>
2022-02-11 21:55:58 +0800

    点击 toggle 开启

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,24 +1,33 @@
 <template>
-  <div class="gulu-dialog-overlay"></div>
-  <div class="gulu-dialog-wrapper">
-    <div class="gulu-dialog">
-      <header>标题 <span class="gulu-dialog-close"></span></header>
-      <main>
-        <p>第一行</p>
-        <p>第二行</p>
-      </main>
-      <footer>
-        <Button level="main">OK</Button>
-        <Button>Cancel</Button>
-      </footer>
+  <template v-if="visible">
+    <div class="gulu-dialog-overlay"></div>
+    <div class="gulu-dialog-wrapper">
+      <div class="gulu-dialog">
+        <header>标题 <span class="gulu-dialog-close"></span></header>
+        <main>
+          <p>第一行</p>
+          <p>第二行</p>
+        </main>
+        <footer>
+          <Button level="main">OK</Button>
+          <Button>Cancel</Button>
+        </footer>
+      </div>
     </div>
-  </div>
+  </template>
+</template>
 </template>
 
 <script lang='ts'>
 import Button from "./Button.vue";
 export default {
   components: { Button },
+  props: {
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+  },
 };
 </script>
 
