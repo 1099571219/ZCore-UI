28764e320b6ee5f732df8955cff2da8db5072a85
lennon <1099571219@qq.com>
2022-02-11 18:06:24 +0800

    Switch css 2

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,15 +1,15 @@
 <template>
-  <button @click="toggle" :class="{ checked:value }"><span></span></button>
-  <div>{{value}}</div>
+  <button @click="toggle" :class="{ checked: value }"><span></span></button>
+  <div>{{ value }}</div>
 </template>
 <script lang='ts'>
 export default {
-    props:{
-        value:Boolean
-    },
-  setup(props,context) {
+  props: {
+    value: Boolean,
+  },
+  setup(props, context) {
     const toggle = () => {
-        context.emit('update:value',!props.value)
+      context.emit("update:value", !props.value);
     };
     return { toggle };
   },
@@ -22,7 +22,7 @@ button {
   height: $h;
   width: $h * 2;
   border: none;
-  background: gray;
+  background: #bfbfbf;
   border-radius: calc($h / 2);
   position: relative;
 }
@@ -34,12 +34,26 @@ span {
   width: $h2;
   background: white;
   border-radius: calc($h2 / 2);
-  transition: left 250ms;
+  transition: all 250ms;
 }
 button.checked {
-  background: blue;
+  background: #1890ff;
 }
 button.checked > span {
   left: calc(100% - #{$h2} - 2px);
+}
+button:focus {
+  outline: none;
+}
+button:active {
+  > span {
+    width: $h2 + 4px;
+  }
+}
+button.checked:active {
+  > span {
+    width: $h2 + 4px;
+    margin-left: -4px;
+  }
 }
 </style>
