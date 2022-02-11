000ca4d3afa1a27daace9d58a5cfa1e3a0e9435d
lennon <1099571219@qq.com>
2022-02-11 16:07:08 +0800

    完善 toggleAside 切换逻辑

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="topnav">
-    <div class="logo" @click="toggleMenu">LOGO</div>
+    <div class="logo">LOGO</div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
-    <span class="toggleAside"></span>
+    <span class="toggleAside" @click="toggleMenu"></span>
   </div>
 </template>
 <script lang="ts">
@@ -16,7 +16,7 @@ export default {
     const toggleMenu = () => {
       menuVisible.value = !menuVisible.value;
     };
-    return{toggleMenu}
+    return { toggleMenu };
   },
 };
 </script>
@@ -41,19 +41,26 @@ export default {
       margin: 0 1em;
     }
   }
-  > .toggleAside{
-      display: inline-block;
-      width:24px;
-      height: 24px;
-      background: red;
-      position: absolute;
-      left:16px;
-      top: 50%;
-      transform: translateY(-50%);
+  > .toggleAside {
+    width: 24px;
+    height: 24px;
+    background: red;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+    display: none;
   }
-  @media (max-width:500px) {
-      >.menu{display: none;}
-      >.logo{margin:0 auto;}
+  @media (max-width: 500px) {
+    > .menu {
+      display: none;
+    }
+    > .logo {
+      margin: 0 auto;
+    }
+    > .toggleAside {
+      display: inline-block;
+    }
   }
 }
 </style>
