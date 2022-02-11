1d974868625c4d89c157af7bff2fe3acad5e335d
lennon <1099571219@qq.com>
2022-02-11 18:32:51 +0800

    创建 Button 组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,3 +1,9 @@
 <template>
-  <div>Button 示例</div>
+  <Button>你好</Button>
 </template>
+<script lang='ts'>
+import Button from '../lib/Button.vue'
+export default {
+  components:{Button}
+}
+</script>
