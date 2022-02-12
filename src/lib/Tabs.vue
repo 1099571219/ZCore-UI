65994670160247926bbeb291423e5e745c2df128
lennon <1099571219@qq.com>
2022-02-12 14:42:10 +0800

    代码优化

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -9,7 +9,7 @@
           v-for="(t, index) in titles"
           :ref="
             (el) => {
-              if (el) navItems[index] = el;
+              if (t === selected) selectedItem = el;
             }
           "
           :key="index"
@@ -41,19 +41,14 @@ export default {
     },
   },
   setup(props, context) {
-    const navItems = ref<HTMLDivElement[]>([]);
+    const selectedItem = ref<HTMLDivElement>(null);
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
     const x = () => {
-      const divs = navItems.value;
-      const result = divs.filter((div) =>
-        div.classList.contains("selected")
-      )[0];
-      console.log(result);
-      const { width } = result.getBoundingClientRect();
+      const { width } = selectedItem.value.getBoundingClientRect();
       indicator.value.style.width = width + "px";
       const { left: left1 } = container.value.getBoundingClientRect();
-      const { left: left2 } = result.getBoundingClientRect();
+      const { left: left2 } = selectedItem.value.getBoundingClientRect();
       const left = left2 - left1;
       indicator.value.style.left = left + "px";
     };
@@ -81,7 +76,7 @@ export default {
       titles,
       current,
       select,
-      navItems,
+      selectedItem,
       indicator,
       container,
     };
