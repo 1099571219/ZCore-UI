0072090c33cfe43336939ec1e67eec8b21cdf86b
lennon <1099571219@qq.com>
2022-02-12 13:59:46 +0800

    遇到 Vue3 Bug

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,15 +1,20 @@
 <template>
   <div>Tabs 示例</div>
   <h1>示例1</h1>
-  <Tabs>
+  <Tabs v-model:selected="x">
     <Tab title="导航1">1</Tab>
     <Tab title="导航2">2</Tab>
   </Tabs>
 </template>
 <script lang='ts'>
-import Tab from '../lib/Tab.vue'
-import Tabs from '../lib/Tabs.vue'
+import { ref } from "vue";
+import Tab from "../lib/Tab.vue";
+import Tabs from "../lib/Tabs.vue";
 export default {
-  components:{Tab,Tabs}  
-}
+  components: { Tab, Tabs },
+  setup() {
+    const x = ref("导航1");
+    return { x };
+  },
+};
 </script>
